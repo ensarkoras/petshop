2d224f286536134cbb62fe4110cdd87afc7953f9
ensarkoras <ensarkoras@gmail.com>
2022-01-10 14:50:46 +0300

    second

---
+++ src/components/Dog.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card color="grey lighten-2">
+    <v-img :src="dog.img" height="170px"></v-img>
+    <v-card-title>
+      <div>
+        <h3>{{dog.name}}</h3>
+        <p class="breed">{{dog.breed}}</p>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    dog: Object,
+  },
+};
+</script>
+
+<style scoped>
+p {
+  margin: 0;
+}
+
+.breed {
+  text-transform: capitalize;
+}
+</style>
